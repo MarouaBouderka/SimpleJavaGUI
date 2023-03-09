8d2e92b43dd0b3baad70b62c0338f34662d2531f
marouab <maroua.bouderka@ensia.edu.dz>
2023-03-09 14:54:11 +0100

    changed msg

--- src/SimpleJavaGUI.java
+++ src/SimpleJavaGUI.java
@@ -3,7 +3,8 @@ public class SimpleJavaGUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hey world");
+		//hello worlld
+		System.out.println("hey world ,,,,");
 	}
 
 }
